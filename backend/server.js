3236dee871c5dbfdb6d4223ce8feac6801658828
Tamas Kiss <kisstii1@gmail.com>
2021-03-01 20:02:39 +0100

    first

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,8 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const app = express();
 
-// default options
-// middleware
 app.use(fileUpload());
 
-// form
 app.use("/form", express.static(__dirname + "/../frontend/index.html"));
 
 app.post("/upload", function (req, res) {
@@ -17,30 +14,19 @@ app.post("/upload", function (req, res) {
     return res.status(400).send("No files were uploaded.");
   }
 
-  sampleFile = req.files.userfile; // form's name parameter
+  sampleFile = req.files.userfile;
   for (let index = 0; index < sampleFile.length; index++) {
-    
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     uploadPath = __dirname + "/upload/" + sampleFile[index].name;
-  
-    // console.log(req.files.userfile);
-    // Use the mv() method to place the file somewhere on your server
+
     sampleFile[index].mv(uploadPath, function (err) {
       if (err) return res.status(500).send(err);
-  
     });
-    
   }
   res.send("File uploaded!");
-  
 });
 
-// serve static
-app.use(express.static('../frontend'));
+app.use(express.static("../frontend"));
 
-// localhost = "127.0.0.1"
 app.listen(8000, "127.0.0.1", () => {
   console.log("Express server listening");
 });
-
-//app.listen(8000); // más is tudja használni, ha tudja az ip-met, csatlakozni tudna a 8000-es portomhoz
--- frontend/script.js
+++ frontend/script.js
@@ -1,36 +1,28 @@
 const pageLoad = () => {
-
   console.log("Client is running");
 
   const form = document.getElementById("uploadForm");
   const fileInputElement = document.getElementById("userfile");
   const userInputElement = document.getElementById("username");
-  
+
   function handleSubmit(e) {
-    e.preventDefault(); // ne küldje el a servernek
+    e.preventDefault();
     console.log("the file upload process is blocked");
     let formData = new FormData();
     formData.append("userfile", fileInputElement.files[0]);
     formData.append("userfile", fileInputElement.files[1]);
     formData.append("username", userInputElement.value);
-    // formData.append("username", "Bela");
-    // console.log(formData.entries());
+
     for (let value of formData.values()) {
       console.log(value);
     }
-    // use http instead of https
-    fetch('/upload', { // 'http://localhost:8000/upload' vagy elég a '/upload'
-    method: 'POST',
-    body: formData,
-    })
 
+    fetch("/upload", {
+      method: "POST",
+      body: formData,
+    });
   }
 
-
   form.addEventListener("submit", handleSubmit);
-  
-  // HTML file input, chosen by user
-  // formData.append("userfile", fileInputElement.files[0]);
-
-}
+};
 window.addEventListener("load", pageLoad);
